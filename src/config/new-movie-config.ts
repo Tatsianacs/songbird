76ead24aec86edb505dedbfd97200d3c6489e8ce
TatsianaMasiukevich <tatsiana.masiukevich@capsilon.com>
2020-08-13 19:04:16 +0300

    feat: add config for app itself and movies

---
+++ src/config/new-movie-config.ts
@@ -0,0 +1,56 @@
+import { Movie } from '../models/movie.model';
+
+export const NEW_MOVIES: Movie[] = [
+  {
+    id: 'n0',
+    firebaseId: '50-en',
+    themoviedbId: '93289',
+    isTv: true
+  },
+  {
+    id: 'n1',
+    firebaseId: '51-en',
+    themoviedbId: '606234'
+  },
+  {
+    id: 'n2',
+    firebaseId: '52-en',
+    themoviedbId: '545609'
+  },
+  {
+    id: 'n3',
+    firebaseId: '53-en',
+    themoviedbId: '475430'
+  },
+  {
+    id: 'n4',
+    firebaseId: '54-en',
+    themoviedbId: '93812',
+    isTv: true
+  },
+  {
+    id: 'n5',
+    firebaseId: '55-en',
+    themoviedbId: '567965'
+  },
+  {
+    id: 'n6',
+    firebaseId: '56-en',
+    themoviedbId: '547016'
+  },
+  {
+    id: 'n7',
+    firebaseId: '57-en',
+    themoviedbId: '480857'
+  },
+  {
+    id: 'n8',
+    firebaseId: '58-en',
+    themoviedbId: '686245'
+  },
+  {
+    id: 'n9',
+    firebaseId: '59-en',
+    themoviedbId: '613504'
+  },
+];
